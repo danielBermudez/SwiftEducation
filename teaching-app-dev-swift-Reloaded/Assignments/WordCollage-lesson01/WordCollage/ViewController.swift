c4e300812bbe62e9a8246dd7fa678c6234cba88e
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-02-22 16:04:59 -0500

    World Collage

--- teaching-app-dev-swift-Reloaded/Assignments/WordCollage-lesson01/WordCollage/ViewController.swift
+++ teaching-app-dev-swift-Reloaded/Assignments/WordCollage-lesson01/WordCollage/ViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var magenta: UIButton!
+    @IBOutlet weak var black: UIButton!
+    @IBOutlet weak var white: UIButton!
+    @IBOutlet weak var Lesson2Label: UILabel!
+    let customColor = UIColor(red: 0, green: 165/200, blue: 100, alpha: 1)
+    
+    @IBAction private func changeBackgroundToBlack(_ sender: Any) {
+        if let button :UIButton = sender as? UIButton{
+            var color = UIColor()
+            if button == white{
+                color = UIColor.white
+            }else if button == black{
+                    color = UIColor.black
+            }else if button == magenta{
+                color = UIColor.magenta}
+            view.backgroundColor = color
+            button.backgroundColor = color
+        }
+        
+       
+
+        
+    }
+ 
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Lesson2Label.textColor = customColor
     }
 
     override func didReceiveMemoryWarning() {
